2739326969bffa5e7ff3d037e8724ebe64da8779
TWKTemplar <carlegos101@gmail.com>
2024-05-05 23:59:28 -0500

    Added console logging to the javascript file

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ window.onload = function () {
     var galleryDiv = document.getElementById('gallery');
 
     fetchImages().then(function (images) {
+        console.log('Fetched images:', images); // Log fetched images
         images.forEach(function (image) {
             var img = document.createElement('img');
             img.src = image.download_url; // Use the download_url directly
             img.alt = image.name;
             galleryDiv.appendChild(img);
         });
+    }).catch(function (error) {
+        console.error('Error fetching images:', error); // Log any errors encountered
     });
 
     function fetchImages() {
+        console.log('Fetching images...'); // Log fetching images
         // Fetch list of images from the GitHub API
         return fetch('https://api.github.com/repos/TWKTemplar/Ch3shireWebsite/contents/images')
             .then(response => response.json())
