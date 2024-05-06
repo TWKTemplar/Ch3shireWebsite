e590084ebf5e8d19f087931326848bca687a023b
TWKTemplar <carlegos101@gmail.com>
2024-05-05 23:57:37 -0500

    sorted for Github API download url

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ window.onload = function () {
     fetchImages().then(function (images) {
         images.forEach(function (image) {
             var img = document.createElement('img');
-            img.src = 'images/' + image.name;
+            img.src = image.download_url; // Use the download_url directly
             img.alt = image.name;
             galleryDiv.appendChild(img);
         });
     });
 
     function fetchImages() {
-        return fetch('images')
+        // Fetch list of images from the GitHub API
+        return fetch('https://api.github.com/repos/TWKTemplar/Ch3shireWebsite/contents/images')
             .then(response => response.json())
-            .then(data => data.filter(item => item.type === 'file'));
+            .then(data => {
+                // Map the response to extract image URLs and names
+                return data.map(item => ({
+                    name: item.name,
+                    download_url: item.download_url // Use download URL for direct image access
+                }));
+            });
     }
 };
